8fc0a73e795de0010cb12ee16a8c7bb3bd62651c
ranc0037 <72412486+ranc0037@users.noreply.github.com>
2021-01-11 23:50:18 -0500

    modified original files

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,22 @@
 'use strict'
 
 // 1. Read the JSON file into a variable called students
+const students = require('./students.json');
 
 // 2. Iterate over the students array and print Hello with their full names to the console
 // e.g. Hello Walter Baker
+students.forEach(students => {
+    console.log(`Hello ${students.firstName + ' ' + students.lastName}`)
+});
+console.log(' ');
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+let count = 0;
+
+for (let i = 0; i < students.length; i++) {
+    if (students[i].lastName.charAt(0).toUpperCase() === 'D') {
+        count = count + 1;
+    }
+}
+console.log('Count of last names starting with D is ' + count + "\n");
